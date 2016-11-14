f555c45275bdf29d24542261e6e4413b3e68a192
Jared Hoberock <jaredhoberock@gmail.com>
2016-11-13 22:45:42 -0600

    Add a partitioner based on the surface area heuristic

--- partitioner.hpp
+++ partitioner.hpp
@@ -60,17 +60,100 @@ struct partition_largest_axis_at_median_element
 
 
   template<class Iterator, class BoundingBox, class Bounder>
-  Iterator operator()(Iterator first, Iterator last, const BoundingBox& box, Bounder bounder) const
+  Iterator operator()(Iterator first, Iterator middle, Iterator last, const BoundingBox& box, Bounder bounder) const
   {
     // create an ordering
     sort_bounding_boxes_by_axis<Bounder> compare(largest_axis(box), bounder);
 
+    std::nth_element(first, middle, last, compare);
+
+    return middle;
+  }
+
+
+  template<class Iterator, class BoundingBox, class Bounder>
+  Iterator operator()(Iterator first, Iterator last, const BoundingBox& box, Bounder bounder) const
+  {
     // split at median element
     Iterator split = first + (last - first) / 2;
 
-    std::nth_element(first, split, last, compare);
+    return operator()(first, split, last, box, bounder);
+  }
+};
 
-    return split;
+
+struct minimize_surface_area_heuristic
+{
+  template<class Iterator, class Bounder>
+  static auto bounding_box(Iterator first, Iterator last, Bounder bounder)
+  {
+    using bounding_box_type = std::result_of_t<Bounder(typename std::iterator_traits<Iterator>::reference)>;
+
+    float inf = std::numeric_limits<float>::infinity();
+    bounding_box_type result{{{inf, inf, inf}, {-inf, -inf, -inf}}};
+        
+    for(; first != last; ++first)
+    {
+      auto bounding_box = bounder(*first);
+
+      for(int i = 0; i < 3; ++i)
+      {
+        result[0][i] = std::min(result[0][i], bounding_box[0][i]);
+        result[1][i] = std::max(result[1][i], bounding_box[1][i]);
+      }
+    }
+
+    return result;
+  }
+
+
+  template<class BoundingBox>
+  static float surface_area(const BoundingBox& box)
+  {
+    float width  = box[1][0] - box[0][0];
+    float height = box[1][1] - box[0][1];
+    float depth  = box[1][2] - box[0][2];
+
+    return 2.f * (width * height + width * depth + height * depth);
+  }
+
+
+  template<class Iterator, class BoundingBox, class Bounder>
+  Iterator operator()(Iterator first, Iterator last, const BoundingBox& box, Bounder bounder) const
+  {
+    size_t num_elements = last - first;
+
+    size_t best_candidate = 0;
+    float smallest_cost = 0;
+
+    partition_largest_axis_at_median_element partitioner;
+
+    std::vector<typename std::iterator_traits<Iterator>::value_type> elements;
+
+    size_t stride = std::max(1ul, num_elements / 1000);
+    for(size_t i = 0; i < (last - first); i += stride)
+    {
+      elements.assign(first, last);
+
+      auto candidate = partitioner(elements.begin(), elements.begin() + i, elements.end(), box, bounder);
+
+      auto left = bounding_box(elements.begin(), candidate, bounder);
+      auto right = bounding_box(candidate, elements.end(), bounder);
+
+      float left_surface_area = surface_area(left);
+      float right_surface_area = surface_area(right);
+
+      float left_cost = float(candidate - elements.begin()) * (left_surface_area / right_surface_area);
+      float right_cost = float(elements.end() - candidate) * (right_surface_area / left_surface_area);
+
+      if(left_cost + right_cost < smallest_cost)
+      {
+        smallest_cost = left_cost + right_cost;
+        best_candidate = candidate - elements.begin();
+      }
+    }
+
+    return partitioner(first, first + best_candidate, last, box, bounder);
   }
 };
 
