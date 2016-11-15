dbf4f09ac4f7146b443415d8d8bb82ae424f0ab0
Jared Hoberock <jaredhoberock@gmail.com>
2016-11-14 23:43:28 -0600

    Add missing #include

--- memoized_bounder.hpp
+++ memoized_bounder.hpp
@@ -1,19 +1,18 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
+#include <iterator>
 #include <type_traits>
 
 template<class T, class Bounder>
 struct memoized_bounder
 {
   // we're memoizing the result of Bounder, which returns a bounding_box_type
-  // that may be different than the type of bounding box used by bounding_volume_hierarchy
   using bounding_box_type = std::result_of_t<Bounder(const T&)>;
-
 
   // avoid copying this thing unintentionally
   memoized_bounder(memoized_bounder&&) = delete;
-
 
   template<class ContiguousRange>
   memoized_bounder(const ContiguousRange& elements, Bounder bounder)
@@ -23,13 +22,11 @@ struct memoized_bounder
     std::transform(elements.begin(), elements.end(), std::back_inserter(bounding_boxes_), bounder);
   }
 
-
   bounding_box_type operator()(const T& element) const
   {
     size_t element_idx = &element - elements_;
     return bounding_boxes_[element_idx];
   }
-
 
   const T* elements_;
   std::vector<bounding_box_type> bounding_boxes_;
